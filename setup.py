a2d8dd5e8ea4d8203dbbdf594ff9aecf031ffcac
slasher125 <hannes.draxl@protonmail.com>
2020-11-18 17:41:36 +0100

    removed dependencies

--- setup.py
+++ setup.py
@@ -9,8 +9,5 @@ setuptools.setup(
         "requests==2.25.0",
         "jupyter==1.0.0",
         "dash==1.17.0",
-        "gunicorn==20.0.4",
-        "heroku3==4.2.3",
-        "heroku_tools==0.3.3",
     ],
 )
